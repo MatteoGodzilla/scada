db25c82db201399307fb28fe67f21e66fc97d998
MatteoGodzilla <catena.matteo90@gmail.com>
2024-06-18 20:28:30 +0200

    added windows for tecnici + other testing files for fxml

--- src/main/java/scada/gui/Test.java
+++ src/main/java/scada/gui/Test.java
@@ -1,10 +1,44 @@
 package scada.gui;
 
-import javafx.scene.control.TextField;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
 
 public class Test extends GuiController {
 
-    public TextField textField;
+    public TreeView<AnchorPane> table;
+
+    public void initialize(){
+
+        // root.getChildren().add(new TreeItem<String>("Figlio 1"));
+        // root.getChildren().add(new TreeItem<String>("Figlio 2"));
+
+        try {
+            TreeItem<AnchorPane> root = new TreeItem<AnchorPane>(
+                FXMLLoader.load(this.getClass().getResource("/row.fxml"))
+            );
+
+            root.getChildren().add(new TreeItem<AnchorPane>(
+                FXMLLoader.load(this.getClass().getResource("/row.fxml"))
+            ));
+
+
+            table.setRoot(root);
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+    }
 
     //this static method cannot be moved in GuiController
     //because static methods can be generic, but not with the generic type defined by the class
@@ -17,10 +51,10 @@ public class Test extends GuiController {
         // Stage stage = new Stage();
         // stage.setScene(inst.getScene());
         // stage.show();
-        System.out.println(textField.textProperty().getValue());
+        // System.out.println(textField.textProperty().getValue());
     }
 
     public void textFieldAction(){
-        System.out.println(textField.textProperty().getValue());
+        // System.out.println(textField.textProperty().getValue());
     }
 }
