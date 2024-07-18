b0796cc9daa69008ba7bbe3deb1b7f576dab1798
Lorenzo Dall'Ara <lorenzo.dallara2@studio.unibo.it>
2024-07-18 11:25:29 +0200

    Modified all GUIs using Stage instead of TitledPane and started implementation.

---
+++ src/main/java/scada/gui/responsabili/ImpiantoCreateController.java
@@ -0,0 +1,14 @@
+package scada.gui.responsabili;
+
+import javafx.stage.Stage;
+import scada.gui.fxml.GuiConstructor;
+import scada.gui.fxml.StageController;
+
+public class ImpiantoCreateController extends StageController {
+    public static ImpiantoCreateController newInstance(){
+        return GuiConstructor.createInstance("/responsabili/ImpiantoCreate.fxml",(ImpiantoCreateController instance, Stage stage) ->{
+            instance.stage = stage;
+        });
+    }
+
+}
---
+++ src/main/java/scada/gui/responsabili/ResponsabiliMain.java
@@ -0,0 +1,16 @@
+package scada.gui.responsabili;
+
+import javafx.stage.Stage;
+import scada.gui.fxml.GuiConstructor;
+import scada.gui.fxml.StageController;
+
+public class ResponsabiliMain extends StageController {
+    private String username;
+
+    public static ResponsabiliMain newInstance(String username) {
+        return GuiConstructor.createInstance("/responsabili/ResponsabileDashboard.fxml", (ResponsabiliMain instance, Stage stage)-> {
+            instance.stage = stage;
+            instance.username = username;
+        });
+    }
+}
